ec88ce15f26633313515770e22086164198aa6c4
Sarah Lessner <sarahlessner@Sarahs-MacBook-Pro.local>
2017-08-15 01:47:14 -0700

    test mail server

--- public/js/contact_me.js
+++ public/js/contact_me.js
@@ -21,7 +21,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "../../views/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
@@ -49,7 +49,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please send a message to sarahlessner@gmail.com!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 var express = require("express");
-// var bodyParser = require("body-parser");
+var bodyParser = require("body-parser");
 var path = require("path");
 
 var app = express()
 var PORT = process.env.PORT || 3030;
 
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.text());
-// app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+
 
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
 
 
 app.use( function(req, res) {
@@ -20,7 +23,29 @@ app.use( function(req, res) {
 	});
 
 
-
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+// must specify options hash even if no options provided!
+var phpExpress = require('php-express')({
+ 
+  // assumes php is in your PATH
+  binPath: 'php'
 });
+ 
+// set view engine to php-express
+app.set('views', './views');
+app.engine('php', phpExpress.engine);
+app.set('view engine', 'php');
+ 
+// routing all .php file to php-express
+app.all(/.+\.php$/, phpExpress.router);
+ 
+var server = app.listen(3030, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('PHPExpress app listening at http://%s:%s', host, port);
+});
+
+
+
+// app.listen(PORT, function() {
+//   console.log("App listening on PORT " + PORT);
+// });
