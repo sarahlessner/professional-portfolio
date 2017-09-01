733b376048fb12f1e56cf63bda00eb6981a433f5
Sarah Lessner <sarahlessner@gmail.com>
2017-08-31 17:03:45 -0700

    attempted to set process.ev variables for email and pw for heroku deployment - testing

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'views')));
 
-var emailpw = require(path.join(__dirname + '/emailpw.js'));
+// var emailpw = require(path.join(__dirname + '/emailpw.js'));
 
 // var emailpw = require("./emailpw.js");
 
